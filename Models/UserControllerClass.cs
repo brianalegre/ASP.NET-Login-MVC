508cd199fd74576417cfc58c0ae3293cb8c8ab34
Brian Alegre <brialegre@yahoo.com>
2022-10-05 22:17:18 -0700

    Add project files.

---
+++ Models/UserControllerClass.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class UserControllerClass
+    {
+        public int Number { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
